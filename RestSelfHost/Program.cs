a7130c3c51fc407eb14dc71be9d865ffc581387f
Tiago Potência <tiagomourabrandao@gmail.com>
2016-07-15 14:55:47 -0300

    Criação do self-host REST com OWIN

---
+++ RestSelfHost/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string baseAddress = "http://localhost:5000/";
+
+            using (WebApp.Start(url: baseAddress))
+            {
+                Console.WriteLine("Programa rodando na porta 5000");
+                System.Threading.Thread.Sleep(-1);
+            }
+
+        }
+    }
+}
---
+++ RestSelfHost/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using SimpleInjector;
+using SimpleInjector.Integration.WebApi;
+
+[assembly: OwinStartup(typeof(RestSelfHost.Startup))]
+
+namespace RestSelfHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+            HttpConfiguration config = new HttpConfiguration();
+
+            config.EnableCors();
+            config.Routes.MapHttpRoute(
+                name: "RestSelfHost",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+                );
+
+            app.UseWebApi(config);
+
+        }
+    }
+}
